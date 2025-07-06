397cd71260e1606287fbed9ff82dd21419d23af8
Edwin Fuquen <efuquen@gmail.com>
2025-07-05 23:41:33 -0600

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,32 @@
+import rehypePrism from '@mapbox/rehype-prism'
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+
+const isProd = process.env.NODE_ENV == 'production'
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/edfu.dev/' : '',
+  basePath: isProd ? '/edfu.dev' : '',
+  output: 'export',
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/articles/*': ['./src/app/articles/**/*.mdx'],
+    },
+  },
+}
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+})
+
+export default withMDX(nextConfig)
---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Edwin Fuquen',
+    default:
+      'Edwin Fuquen - Software engineer, creator, and craftsman.',
+  },
+  description:
+    "I’m Edwin, a software engineer who lives in Denver, Colorado. Currently I lead a team on Google Maps responsible for getting satellite and aerial imagery onto Maps and other Google products.",
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
